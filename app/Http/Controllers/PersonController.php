03686b437ae123ebfc4190e6dc49d199dbde797c
unknown <mkostafa79@gmail.com>
2020-05-05 09:14:54 +0200

    intaial commit

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PersonRescource;
+use App\Http\Resources\PersonRescourceCollection;
+use App\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+
+    public function show(Person $person): PersonRescource
+    {
+
+        return new PersonRescource($person);
+    }
+    public function index(): PersonRescourceCollection
+    {
+
+        return new PersonRescourceCollection(Person::paginate());
+    }
+}
